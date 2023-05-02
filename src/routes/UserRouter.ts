cf78418d2dbdf89f4986bdf6c5ef01d6a8346407
Victor Henrique <92759987+Victorh2s@users.noreply.github.com>
2023-05-02 11:57:23 -0300

    CI

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -4,11 +4,9 @@ import { CreateUser } from "../http/controllers/UserController/CreateUser";
 import { UpdateUser } from "../http/controllers/UserController/UpdateUser";
 import { DeleteUser } from "../http/controllers/UserController/DeleteUser";
 import { AuthMiddleware } from "../middlewares/auth";
-import { GetAllUsersDev } from "../http/controllers/UserController/GetAllUsersDev";
 
 export const RouteUser = Router();
 
-RouteUser.get("/getalluser", GetAllUsersDev);
 RouteUser.get("/", AuthMiddleware, GetUser);
 RouteUser.post("/", CreateUser);
 RouteUser.put("/", AuthMiddleware, UpdateUser);
